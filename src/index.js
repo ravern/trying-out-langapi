a433ef0580d8374dc59695760927f933b938ee24
Ravern Koh <ravernkoh@gmail.com>
2019-10-23 15:58:44 +0800

    Remove `langapi` and switch to Crowdin integration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { LangProvider, Tr } from 'react-langapi';
-
-import { LangClient as client } from './langapi/LangClient';
 
 function App() {
   const [language, setLanguage] = useState('es');
   
   useEffect(() => {
-    client.setForceLanguage(language);
+    // TODO: Change the language
   }, [language]);
 
   return <LangProvider client={client}>
     <select onChange={e => setLanguage(e.target.value)} value={language}>
       <option>en</option>
-      <option>es</option>
       <option>zh</option>
     </select>
-    <h1><Tr>Hello, world!</Tr></h1>
-    <h2><Tr variables={{ name: 'Ravern' }}>I am {'{name}'}.</Tr></h2>
+    <h1>Hello, world!</h1>
+    <h2>I am Ravern.</h2>
   </LangProvider>
 }
 
